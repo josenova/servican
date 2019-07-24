0e6bd56703c473f90530400f5828a0ca62c92dc6
Jose Nova <josenova@Joses-MacBook-Pro.local>
2019-07-24 16:00:32 -0400

    remove force www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :force_www!
+  # before_filter :force_www!
 
   def after_sign_in_path_for(resource)
     admin_clients_path
